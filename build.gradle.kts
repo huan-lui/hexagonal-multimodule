7f4a722e50dd7b613c0e7b96ba74be97bc92b747
🐌Juan Luis García López <juan.garcia.lopez@nexthink.com>
2024-10-28 16:16:17 +0100

    Share configs between submodules

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,19 +2,27 @@ plugins {
     id("java")
 }
 
+subprojects{ apply(plugin = "java") }
+
 group = "org.huanlui.bank"
 version = "1.0-SNAPSHOT"
 
-repositories {
-    mavenCentral()
+allprojects {
+    repositories {
+        mavenCentral()
+    }
+
+    dependencies {
+        testImplementation(platform("org.junit:junit-bom:5.9.1"))
+        testImplementation("org.junit.jupiter:junit-jupiter")
+        testImplementation("org.assertj:assertj-core:3.26.3")
+    }
+
+    tasks.test {
+        useJUnitPlatform()
+    }
 }
 
-dependencies {
-    testImplementation(platform("org.junit:junit-bom:5.9.1"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
-}
 
-tasks.test {
-    useJUnitPlatform()
-}
+
 
